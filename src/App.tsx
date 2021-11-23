9d2573badb45ad53534cb32b4eea1317121d0e94
Billy Chui <billy2022002@gmail.com>
2021-11-23 17:06:11 +1100

    create react project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import style from './App.module.scss';
+import Counter from './screens/Counter';
+
+const App = () => (
+    <div className={style.App}>
+        <header className={style['App-header']}>
+            <Counter />
+            <img src={logo} className={style['App-logo']} alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className={style['App-link']}
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    </div>
+);
+
+export default App;
