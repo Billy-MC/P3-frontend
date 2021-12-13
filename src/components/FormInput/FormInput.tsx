84f8f16470b2018f6073595e3ed36947723216d5
Billy Chui <billy2022002@gmail.com>
2021-12-13 12:49:44 +1100

    registration form

---
+++ src/components/FormInput/FormInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './FormInput.css';
+
+type FormInputProps = {
+    id: number;
+    label: string;
+    name: string;
+    placeholder: string;
+    errorMessage: string;
+    required: boolean;
+    type: string;
+    pattern: string;
+    handleChange: React.ChangeEvent<HTMLInputElement>;
+    // handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const FormInput = (props: FormInputProps) => {
+    const [focused, setFocused] = useState(false);
+
+    const focusHandler = () => {
+        setFocused(true);
+    };
+
+    const { label, errorMessage, handleChange, id, ...inputProps } = props;
+    return (
+        <div className="formInput">
+            <label htmlFor="">{label}</label>
+            <input
+                {...inputProps}
+                onChange={handleChange}
+                onFocus={() => inputProps.name === 'confirmPassword' && setFocused(true)}
+                focused={focused.toString()}
+                onBlur={focusHandler}
+            />
+            <span>{errorMessage}</span>
+        </div>
+    );
+};
+
+export default FormInput;
