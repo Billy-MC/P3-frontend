0e14e4cfdac62d29a34147d104c8b620ccc3b534
Billy Chui <billy2022002@gmail.com>
2021-12-14 14:05:03 +1100

    login page first draft

--- src/screens/LoginPage/LoginPage.tsx
+++ src/screens/LoginPage/LoginPage.tsx
@@ -1,7 +1,70 @@
-const LoginPage = () => (
-    <div>
-        <p>LoginPage</p>
-    </div>
-);
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default LoginPage;
+import './LoginPage.scss';
+import RegisterForm from '../../components/User/RegisterForm/RegisterForm';
+
+const initialState = {
+    email: '',
+    password: '',
+};
+
+const RegisterPage = () => {
+    const [values, setValues] = useState(initialState);
+    const inputs = [
+        {
+            id: 1,
+            name: 'email',
+            type: 'text',
+            placeholder: 'Email',
+            errorMessage: 'It should be a valid email address!',
+            label: 'Email',
+            required: true,
+        },
+        {
+            id: 2,
+            name: 'password',
+            type: 'password',
+            placeholder: 'Password',
+            errorMessage:
+                'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^_&*])[a-zA-Z0-9!@#$%^_&*]{8,20}$`,
+            label: 'Password',
+            required: true,
+        },
+    ];
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setValues(initialState);
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setValues({ ...values, [e.currentTarget.name]: e.currentTarget.value });
+    };
+
+    return (
+        <div className="loginform">
+            <div className="loginform-box">
+                <div className="loginform-title">Devil CRM System</div>
+                <form onSubmit={submitHandler}>
+                    <h1>Login to your account</h1>
+                    {inputs.map((input) => (
+                        <RegisterForm key={input.id} {...input} onChange={onChange} />
+                    ))}
+                    <button type="submit">LOGIN</button>
+                    <p>
+                        {' '}
+                        <Link to="/forgetpassword">Forgot password?</Link>
+                    </p>
+                    <p>
+                        {' '}
+                        Don&lsquo;t have an account? &nbsp;
+                        <Link to="/register">Sign up</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+};
+export default RegisterPage;
