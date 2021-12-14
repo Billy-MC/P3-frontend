055a71942bc52876b1cd1d1d363ccab9a43d093f
Benny.Dong <mr.d@live.com>
2021-12-14 19:12:36 +1100

    sidebar draft

--- src/layouts/sideNav/SideNav.tsx
+++ src/layouts/sideNav/SideNav.tsx
@@ -2,35 +2,37 @@ import { Link } from 'react-router-dom';
 
 const SideNav = () => (
     <div>
-        <ul>
-            <li>
-                <Link to="/">Welcome</Link>
-            </li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-            <li>
-                <Link to="/register">Register</Link>
-            </li>
-            <li>
-                <Link to="/forgetpassword">Forget Password</Link>
-            </li>
-            <li>
-                <Link to="/customer">Customer</Link>
-            </li>
-            <li>
-                <Link to="/user">User</Link>
-            </li>
-            <li>
-                <Link to="/product">Product</Link>
-            </li>
-            <li>
-                <Link to="/order">Order</Link>
-            </li>
-            <li>
-                <Link to="/dashboard">Dashboard</Link>
-            </li>
-        </ul>
+        <nav id="sidebar-menu">
+            <ul id="sidebar-toggle" className="side-menu">
+                <li>
+                    <Link to="/">Welcome</Link>
+                </li>
+                <li className="active">
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/forgetpassword">Forget Password</Link>
+                </li>
+                <li>
+                    <Link to="/customer">Customer</Link>
+                </li>
+                <li>
+                    <Link to="/user">User</Link>
+                </li>
+                <li>
+                    <Link to="/product">Product</Link>
+                </li>
+                <li>
+                    <Link to="/order">Order</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
+        </nav>
     </div>
 );
 export default SideNav;
