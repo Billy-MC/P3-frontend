c76c44145b5f8d56e4abf092fd43ec5409b6dfb8
Billy Chui <billy2022002@gmail.com>
2021-12-14 14:22:26 +1100

    confirm dont break the page

--- src/components/User/Form/Form.tsx
+++ src/components/User/Form/Form.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Form.scss';
 
-type RegisterFormProps = {
+type FormProps = {
     label: string;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
     errorMessage: string;
     name: string;
 };
 
-const RegisterForm = (props: RegisterFormProps) => {
+const Form = (props: FormProps) => {
     const { label, onChange, errorMessage, ...inputProps } = props;
     return (
         <div className="formInput">
@@ -19,4 +19,4 @@ const RegisterForm = (props: RegisterFormProps) => {
     );
 };
 
-export default RegisterForm;
+export default Form;
