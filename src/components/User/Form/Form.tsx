fd89a028e5f0860792844d14689079250c684a20
Billy Chui <billy2022002@gmail.com>
2021-12-14 16:44:18 +1100

    extra div

--- src/components/User/Form/Form.tsx
+++ src/components/User/Form/Form.tsx
@@ -11,12 +11,10 @@ type FormProps = {
 const Form = (props: FormProps) => {
     const { label, onChange, errorMessage, ...inputProps } = props;
     return (
-        <div>
-            <div className="formInput">
-                <label>{label}</label>
-                <input {...inputProps} onChange={onChange} />
-                <span>{errorMessage}</span>
-            </div>
+        <div className="formInput">
+            <label>{label}</label>
+            <input {...inputProps} onChange={onChange} />
+            <span>{errorMessage}</span>
         </div>
     );
 };
