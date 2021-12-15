47f6e4605cf80daee43db29265a8ada2a465f7c9
Billy Chui <billy2022002@gmail.com>
2021-12-16 01:41:14 +1100

    change the color

--- src/screens/LoginPage/LoginPage.tsx
+++ src/screens/LoginPage/LoginPage.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography, FormControlLabel, Button, Checkbox } from '@mui/material';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import loginStyles from './LoginPage.module.scss';
 import Form from '../../components/User/Form/Form';
 import PasswordForm from '../../components/User/Form/PasswordForm';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#08192d',
+        },
+        secondary: {
+            main: '#00fff0',
+        },
+    },
+    typography: {
+        fontSize: 12,
+    },
+});
 
 const LoginPage = () => {
     const [checked, setChecked] = useState(false);
@@ -38,9 +52,11 @@ const LoginPage = () => {
                 noValidate
                 autoComplete="off"
             >
-                <Typography className={loginStyles.login_title} component="h1" variant="h5">
-                    Login to your account
-                </Typography>
+                <ThemeProvider theme={theme}>
+                    <Typography className={loginStyles.login_title} component="h1" variant="h5">
+                        Login to your account
+                    </Typography>
+                </ThemeProvider>
                 {inputs.map((input) => (
                     <Form key={input.id} {...input} label={input.label} />
                 ))}
@@ -59,11 +75,18 @@ const LoginPage = () => {
                     }
                     label={<Typography variant="subtitle1">Remember me</Typography>}
                 />
-                <Link className={loginStyles.login_Btn} to="/dashboard">
-                    <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                        Login
-                    </Button>
-                </Link>
+                <ThemeProvider theme={theme}>
+                    <Link className={loginStyles.login_Btn} to="/dashboard">
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, backgroundColor: 'primary' }}
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                </ThemeProvider>
                 <Box
                     className={loginStyles.login_bottom}
                     sx={{ display: 'flex', flexDirection: 'column' }}
