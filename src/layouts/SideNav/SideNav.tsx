5e2238e82d651b3bf1aea2727a0e90e1c02e4dae
DavinJ-0316 <davinjiang95@gmail.com>
2021-12-16 20:49:35 +1100

    added FC and ReactNode types

--- src/layouts/SideNav/SideNav.tsx
+++ src/layouts/SideNav/SideNav.tsx
@@ -4,21 +4,22 @@ const SideNav = () => (
     <div>
         <ul>
 <<<<<<< HEAD:src/layouts/SideNav/SideNav.tsx
-            <li>
+<<<<<<< HEAD:src/layouts/SideNav/SideNav.tsx
+<li>
 ||||||| constructed merge base:src/layouts/sideNav/SideNav.tsx
             <li>
-                <Link to="/">Welcome</Link>
-            </li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-            <li>
-                <Link to="/register">Register</Link>
-            </li>
-            <li>
-                <Link to="/forgetpassword">Forget Password</Link>
-            </li>
-            <li>
+        <Link to="/">Welcome</Link>
+    </li>
+    <li>
+        <Link to="/login">Login</Link>
+    </li>
+    <li>
+        <Link to="/register">Register</Link>
+    </li>
+    <li>
+        <Link to="/forgetpassword">Forget Password</Link>
+    </li>
+    <li>
 =======
             {/* <li>
                 <Link to="/">Welcome</Link>
@@ -32,22 +33,37 @@ const SideNav = () => (
             <li>
                 <Link to="/forgetpassword">Forget Password</Link>
             </li> */}
+||||||| constructed merge base:src/layouts/sideNav/SideNav.tsx
+            {/* <li>
+                <Link to="/">Welcome</Link>
+            </li>
             <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/forgetpassword">Forget Password</Link>
+            </li> */}
+=======
+>>>>>>> added FC and ReactNode types:src/layouts/sideNav/SideNav.tsx
+        <li>
 >>>>>>> all problems related to sideBar solved:src/layouts/sideNav/SideNav.tsx
-                <Link to="/customer">Customer</Link>
-            </li>
-            <li>
-                <Link to="/user">User</Link>
-            </li>
-            <li>
-                <Link to="/product">Product</Link>
-            </li>
-            <li>
-                <Link to="/order">Order</Link>
-            </li>
-            <li>
-                <Link to="/dashboard">Dashboard</Link>
-            </li>
+            <Link to="/customer">Customer</Link>
+        </li>
+        <li>
+            <Link to="/user">User</Link>
+        </li>
+        <li>
+            <Link to="/product">Product</Link>
+        </li>
+        <li>
+            <Link to="/order">Order</Link>
+        </li>
+        <li>
+            <Link to="/dashboard">Dashboard</Link>
+        </li>
         </ul>
     </div>
 );
