0dfbcd1ce2e7e8b7b8bb33110f314fd024d3d561
samztz <1196572086@qq.com>
2021-12-16 18:12:46 +1100

    rebase to main

--- src/screens/WelcomePage/WelcomePage.tsx
+++ src/screens/WelcomePage/WelcomePage.tsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import welcomeHeading from './components/welcomeHeading';
 
 const WelcomePage = () => (
     <div>
-        <Link to="/Login">Welcome</Link>
+        <welcomeHeading />
+        <Link to="/login">
+            <Button>Welcome</Button>
+        </Link>
+        <Link to="/register">
+            <Button>Register</Button>
+        </Link>
     </div>
 );
 
---
+++ src/screens/WelcomePage/components/welcomeHeading.tsx
@@ -0,0 +1,11 @@
+import Stack from '@mui/material/Button';
+
+const welcomeHeading = () => (
+    <div>
+        <Stack>
+            <div>logo1</div>
+            <div>logo2</div>
+        </Stack>
+    </div>
+);
+export default welcomeHeading;
