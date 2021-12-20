1110a86e35d8debd3a76d03fa8d13d09a9525051
samztz <1196572086@qq.com>
2021-12-20 20:00:51 +1100

    fixed problems in code review

--- src/components/HeadingWithSection/HeadingWithSection.tsx
+++ src/components/HeadingWithSection/HeadingWithSection.tsx
@@ -2,21 +2,16 @@ import * as react from 'react';
 import styles from './headingWithSection.module.scss';
 
 interface IHeadingWithSection {
-    Heading: react.FC<{}>;
-    Section: react.FC<{}>;
+    heading: react.ReactNode;
+    section: react.ReactNode;
 }
 
 const HeadingWithSection: react.FC<IHeadingWithSection> = (props) => {
-    const { Heading, Section } = props;
-
+    const { heading, section } = props;
     return (
         <div className={styles.headingWithSection}>
-            <div className={styles.heading}>
-                <Heading />
-            </div>
-            <div className={styles.section}>
-                <Section />
-            </div>
+            <div className={styles.heading}>{heading}</div>
+            <div className={styles.section}>{section}</div>
         </div>
     );
 };
--- src/screens/WelcomePage/WelcomePage.tsx
+++ src/screens/WelcomePage/WelcomePage.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import HeadingWithSection from '../../components/HeadingWithSection';
 import WelcomeHeading from './components/WelcomeHeading';
 import WelcomeSection from './components/WelcomeSection';
+import styles from './welcomePage.module.scss';
 
-const WelcomePage = () => (
-    <div>
-        <HeadingWithSection Heading={WelcomeHeading} Section={WelcomeSection} />
+const WelcomePage: React.FC = () => (
+    <div className={styles.welcomePage}>
+        <HeadingWithSection heading={<WelcomeHeading />} section={<WelcomeSection />} />
     </div>
 );
 
