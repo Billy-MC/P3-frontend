8ecef2f6e1ba0488ae551ceba1b6b18b532c94a5
samztz <1196572086@qq.com>
2021-12-20 20:25:24 +1100

    remove components_division.txt

--- src/screens/WelcomePage/components/WelcomeHeading/WelcomeHeading.tsx
+++ src/screens/WelcomePage/components/WelcomeHeading/WelcomeHeading.tsx
@@ -1,12 +1,13 @@
 import * as react from 'react';
 import styles from './welcomeHeading.module.scss';
+const a = 1;
 
 const WelcomeHeading: react.FC = () => (
     <div className={styles.welcomeHeading}>
         <div className={styles.logo}>
             <p>logo</p>
         </div>
-        <div className={styles.company}>
+        <div className={a === 1 && styles.company}>
             <h2>DevilsCRM</h2>
         </div>
     </div>
