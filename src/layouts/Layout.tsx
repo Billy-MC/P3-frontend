16d510dc0c69701fdb66a91625a75291c0377956
DavinJ-0316 <davinjiang95@gmail.com>
2021-12-20 21:02:34 +1100

    added layout for router

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from './Header';
+import SideNav from './SideNav';
+import Footer from './Footer';
+
+const Layout = () => (
+    <div>
+        <Header />
+        <SideNav />
+        <Footer />
+    </div>
+);
+
+export default Layout;
