333bc089f86fb0a3a8c4a4f24e11ca2714f37473
Benny.Dong <mr.d@live.com>
2021-12-23 20:42:59 +1100

    Adjust CSS Style based on Datum Site

--- src/layouts/sideNav/SideNav.tsx
+++ src/layouts/sideNav/SideNav.tsx
@@ -11,11 +11,12 @@ import {
 } from '@mui/material';
 
 import {
-    Home as Dashboard,
-    People as Customer,
-    ShoppingBasket as Product,
-    ShoppingCart as Order,
-    Person as User,
+    HomeOutlined as Dashboard,
+    CloseOutlined as Close,
+    PeopleOutlined as Customer,
+    ShoppingBasketOutlined as Product,
+    ShoppingCartOutlined as Order,
+    PersonOutlined as User,
 } from '@mui/icons-material';
 
 import styles from './SideNav.module.scss';
@@ -27,27 +28,27 @@ const SideNav = () => {
     const menuItems = [
         {
             text: 'Dashboard',
-            icon: <Dashboard color="secondary" />,
+            icon: <Dashboard style={{ fill: '#8f9fbc' }} />,
             path: '/dashboard',
         },
         {
             text: 'Customers',
-            icon: <Customer color="secondary" />,
+            icon: <Customer style={{ fill: '#8f9fbc' }} />,
             path: '/customer',
         },
         {
             text: 'Products',
-            icon: <Product color="secondary" />,
+            icon: <Product style={{ fill: '#8f9fbc' }} />,
             path: '/product',
         },
         {
             text: 'Orders',
-            icon: <Order color="secondary" />,
+            icon: <Order style={{ fill: '#8f9fbc' }} />,
             path: '/order',
         },
         {
             text: 'Users',
-            icon: <User color="secondary" />,
+            icon: <User style={{ fill: '#8f9fbc' }} />,
             path: '/user',
         },
     ];
@@ -60,21 +61,29 @@ const SideNav = () => {
                 anchor="left"
                 classes={{ paper: styles.drawerPaper }}
             >
-                <div>
-                    <Typography variant="h5" className={styles.title}>
-                        CRM - Devils
+                <div className={styles.sidebarHeader}>
+                    <div className={styles.sidebarLogo}>
+                        <p>logo</p>
+                    </div>
+                    <Typography variant="h5" className={styles.sidebarTitle}>
+                        Devils
                     </Typography>
+                    <div className={styles.sidebarClose}>
+                        <Close />
+                    </div>
                 </div>
                 <List>
                     {menuItems.map((item) => (
-                        <ListItem
-                            key={item.text}
-                            onClick={() => navigate(item.path)}
-                            className={location.pathname === item.path ? styles.active : undefined}
-                        >
-                            <ListItemButton>
+                        <ListItem>
+                            <ListItemButton
+                                key={item.text}
+                                onClick={() => navigate(item.path)}
+                                className={
+                                    location.pathname === item.path ? styles.active : undefined
+                                }
+                            >
                                 <ListItemIcon>{item.icon}</ListItemIcon>
-                                <ListItemText primary={item.text} />
+                                <ListItemText primary={item.text} className={styles.listItemText} />
                             </ListItemButton>
                         </ListItem>
                     ))}
