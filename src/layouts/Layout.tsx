f1e77c1e587269f169208da611ac452d42711029
Billy Chui <billy2022002@gmail.com>
2022-01-04 01:25:28 +0000

    Merged in bugfix/DEV-88-layout-padding (pull request #34)
    padding left,top
    
    * padding left,top
    
    * padding left,top
    
    * remove layout.module, and rewrite in index.scss
    
    * remove layout.module, and rewrite in index.scss
    
    * padding left,top
    
    * remove layout.module, and rewrite in index.scss
    
    * padding left,top
    
    * recreate layout.module

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode, FC, Fragment } from 'react';
 import SideNav from './SideNav';
 import Header from './Header';
 import Footer from './Footer';
+import styles from './Layout.module.scss';
 
 type LayoutProps = {
     children: ReactNode;
@@ -11,7 +12,7 @@ const Layout: FC<LayoutProps> = ({ children }) => (
     <>
         <Header />
         <SideNav />
-        {children}
+        <div className={styles.main}>{children}</div>
         <Footer />
     </>
 );
