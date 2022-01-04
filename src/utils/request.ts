d846a4b69f1d472959483c67d22f972ea650614d
Billy Chui <billy2022002@gmail.com>
2022-01-04 23:21:11 +0000

    request.ts edited online with Bitbucket

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000 ' : '';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
 const withCredentials = true;
 const timeout = 30000;
 
