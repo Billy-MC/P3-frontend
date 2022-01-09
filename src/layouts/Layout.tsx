8a7840553a569c1c61eb2ccb0e76dfd03c23dc09
Billy Chui <billy2022002@gmail.com>
2022-01-09 18:50:38 +1100

    remove footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode, FC, Fragment } from 'react';
 import SideNav from './SideNav';
 import Header from './Header';
-import Footer from './Footer';
 import styles from './Layout.module.scss';
 
 type LayoutProps = {
