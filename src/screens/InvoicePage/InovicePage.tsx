c6249e1fd56db5ffb40c6f5acb72c7d57094693f
Tingzhuang Zhou <tingzhuangzhou@gmail.com>
2022-01-23 13:34:53 +0000

    Merged in bugfix/DEV-169-fix-cant-build-bug (pull request #86)
    fixed can't pipeline bug
    
    * fixed can't pipeline bug

--- src/screens/InvoicePage/InovicePage.tsx
+++ src/screens/InvoicePage/InovicePage.tsx
@@ -2,7 +2,6 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import styles from './InvoicePage.module.scss';
 import InvoiceSearchBar from './components/InvoiceSearchBar';
-import InvoiceDataGrid from './components/InvoiceDataGrid';
 
 const InvoicePage = () => (
     <div>
@@ -11,7 +10,6 @@ const InvoicePage = () => (
         </Box>
         <Box className={styles['invoice-box_white']}>
             <InvoiceSearchBar />
-            <InvoiceDataGrid />
         </Box>
     </div>
 );
